841df01a6964d0a7f7faf95131a327ea94ed6172
Julio Lopes <juliolopes.bfg@gmail.com>
2022-12-19 12:29:51 -0300

    reworked form

--- src/App.jsx
+++ src/App.jsx
@@ -7,64 +7,23 @@ import {
   Box,
   Text,
   Stack,
-  useToast,
 } from "@chakra-ui/react";
 
 const App = () => {
-  const [editable, setEditable] = React.useState(true);
-  const [copied, setCopied] = React.useState(false);
-  const [hasSignature, setHasSignature] = React.useState(false);
-  const [signature, setSignature] = React.useState('');
-  const [shallowSignature, setShallowSignature] = React.useState('');
 
-  const toast = useToast()
+  const [signature, setSignature] = React.useState("Surgery Left : Prohylatic (preventive) Mastectomy");
 
-  const Toast = () => {
-    return (
-      <a onClick={handleSignatureSubmit}>
-        <Button
-        colorScheme="blue"
-        marginBlock={2}
-          onClick={() =>
-            toast({
-              title: 'Signature Saved!',
-              status: 'success',
-              duration: 3000,
-              isClosable: true,
-            })
-          }
-        >
-          Save
-        </Button>
-      </a>
-    )
-  }
-
-
-  const handleSignatureButton = () => {
-    setHasSignature(!hasSignature);
-  };
-
-  const handleSignature = (e) => {
-    setShallowSignature(e);
-  };
-
-  const handleSignatureSubmit = (e) => {
-     setSignature(shallowSignature);
-  }
-
-  const copyToClipBoard = async copyMe => {
+  const copyToClipBoard = async (copyMe) => {
     try {
       await navigator.clipboard.writeText(copyMe);
-      setCopySuccess('Copied!');
-      
+      setCopySuccess("Copied!");
     } catch (err) {
-      setCopySuccess('Failed to copy!');
+      setCopySuccess("Failed to copy!");
     }
   };
 
   return (
-    <Box  w="100%" p={4} color="black">
+    <Box w="100%" p={4} color="black">
       <Container padding={4}>
         <Stack spacing={3}>
           <Text fontSize="3xl" color="black">
@@ -78,116 +37,48 @@ const App = () => {
           <Text as="b" fontSize="lg" color="black">
             You must have Tx and Dx to generate your signature.
           </Text>
+          <a href="https://www.breastcancer.org/profile?section=your-treatments">
+            <Button marginBlock={2} colorScheme="facebook">
+              Add Tx
+            </Button>
+          </a>
+          <a href="https://www.breastcancer.org/profile?section=your-diagnoses">
+            <Button marginBlock={2} colorScheme="facebook">
+              Add Dx
+            </Button>
+          </a>
           <Text color="black" fontSize="xl">
             Add a signature to your comments by following these steps:
           </Text>
           <Text color="black" fontSize="lg" as="b">
-            1. Copy Signature Summary
+            1. Copy Signature
           </Text>
         </Stack>
       </Container>
-      {hasSignature ? (
-        <Container padding={4}>
-          <Textarea disabled placeholder="Current Signature" value={signature} />
-          <Stack spacing={3}>
-            <Text fontSize="lg" color="black" as="b">
-              Update Your Signature
-            </Text>
-            <Box>
-              <Text fontSize="xl" color="black">
-                1. Visit your Signature Summary here
-              </Text>
-              <Text fontSize="xl" color="black">
-                2. Copy your Signature
-              </Text>
-              <Text fontSize="xl" color="black">
-                3. Paste it in the box below
-              </Text>
-              <Text fontSize="xl" color="black">
-                4. Save
-              </Text>
-            </Box>
-            <Text fontSize="xl" color="black" as="b">
-              Signature
-            </Text>
-            <Textarea
-              editable={editable}
-              placeholder="Paste your signature here"
-              
-              onChange={e=> handleSignature(e.target.value)}
-            />
-            <Toast/>
-          </Stack>
-        </Container>
-      ) : (
-        <Container padding={4}>
-          <Textarea editable={editable} onChange={e=> setSignature(e.target.value)} />
-          <a 
-            onClick={() =>
-              toast({
-                title: 'Signature Copied!',
-                status: 'success',
-                duration: 3000,
-                isClosable: true,
-              })
-            }
+      <Container padding={4}>
+        <Textarea
+          isDisabled
+          value="Surgery Left : Prohylatic (preventive) Mastectomy"
+        />
+        <Stack>
+          <a
+            href="https://breastcancer.vanillastaging.com/en/profile/signature"
+            rel="noopener"
+            target="_blank"
           >
-            <Button marginBlock={2} colorScheme="blue" onClick={() => copyToClipBoard(signature)}>
-              Copy
+            <Button
+              colorScheme="facebook"
+              marginBlock={3}
+              onClick={() => copyToClipBoard("Surgery Left : Prohylatic (preventive) Mastectomy")}
+            >
+              <Text>
+                <Text as="b"> Paste </Text>
+                In Community Signature
+              </Text>
             </Button>
           </a>
-          {copied && (
-            <Text fontSize="lg" as="b" color="green">
-              Copied!
-            </Text>
-          )}
-          <Stack>
-            <Text color="black" fontSize="lg" as="b">
-              2. Paste signature in your{" "}
-              <Text
-                color="black"
-                fontSize="lg"
-                as="b"
-                textDecoration={"underline"}
-              >
-                <a href="https://breastcancer.vanillastaging.com/en/profile/signature">Community Signature</a>
-              </Text>{" "}
-              page
-            </Text>
-            <Text
-              color="black"
-              fontSize="lg"
-              as="b"
-              textDecoration={"underline"}
-            >
-              <a href="/summary">Signature Summary</a>
-            </Text>
-            <Text
-              color="black"
-              fontSize="lg"
-              as="b"
-              textDecoration={"underline"}
-            >
-              <a href="/tx">Add Tx</a>
-            </Text>
-            <Text
-              color="black"
-              fontSize="lg"
-              as="b"
-              textDecoration={"underline"}
-            >
-              <a href="/dx">Add Dx</a>
-            </Text>
-          </Stack>
-        </Container>
-      )}
-      <Button
-        marginBlock={2}
-        colorScheme="blue"
-        onClick={handleSignatureButton}
-      >
-        Empty/ or W signature
-      </Button>
+        </Stack>
+      </Container>
     </Box>
   );
 };
